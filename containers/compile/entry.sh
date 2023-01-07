536a323056a2202732e3b3526ebd9dc2145586de
diff3 <magnus@entropy.nu>
2023-01-07 21:24:13 +0100

    bug fixes autoconf, worldserver.conf

--- containers/compile/entry.sh
+++ containers/compile/entry.sh
@@ -40,20 +40,19 @@ sed -i -e "/LoginDatabaseInfo =/ s/= .*/= \"$(escape $DB_CONTAINER)\;3306\;$(esc
 
 # worldserver.conf
 sed -i -e "/DataDir =/ s/= .*/= $(escape $DATA_DIR_PATH)/" $CONFIG_PATH/worldserver.conf
-sed -i -e "/Warden.ModuleDir             =/ s/= .*/= $(escape $WARDEN_DIR_PATH)/" $CONFIG_PATH/worldserver.conf
+sed -i -e "/LogsDir =/ s/= .*/= $(escape $LOGS_DIR_PATH)/" $CONFIG_PATH/worldserver.conf
 
-sed -i -e "/LoginDatabase.Info              =/ s/= .*/= \"$(escape $DB_CONTAINER)\;3306\;$(escape $SERVER_DB_USER)\;$(escape $SERVER_DB_PWD)\;realmd\"/" $CONFIG_PATH/worldserver.conf
-sed -i -e "/WorldDatabase.Info              =/ s/= .*/= \"$(escape $DB_CONTAINER)\;3306\;$(escape $SERVER_DB_USER)\;$(escape $SERVER_DB_PWD)\;worldserver\"/" $CONFIG_PATH/worldserver.conf
-sed -i -e "/CharacterDatabase.Info          =/ s/= .*/= \"$(escape $DB_CONTAINER)\;3306\;$(escape $SERVER_DB_USER)\;$(escape $SERVER_DB_PWD)\;characters\"/" $CONFIG_PATH/worldserver.conf
-sed -i -e "/LogsDatabase.Info               =/ s/= .*/= \"$(escape $DB_CONTAINER)\;3306\;$(escape $SERVER_DB_USER)\;$(escape $SERVER_DB_PWD)\;logs\"/" $CONFIG_PATH/worldserver.conf
+sed -i -e "/LoginDatabaseInfo     =/ s/= .*/= \"$(escape $DB_CONTAINER)\;3306\;$(escape $SERVER_DB_USER)\;$(escape $SERVER_DB_PWD)\;auth\"/" $CONFIG_PATH/worldserver.conf
+sed -i -e "/WorldDatabaseInfo     =/ s/= .*/= \"$(escape $DB_CONTAINER)\;3306\;$(escape $SERVER_DB_USER)\;$(escape $SERVER_DB_PWD)\;world\"/" $CONFIG_PATH/worldserver.conf
+sed -i -e "/CharacterDatabaseInfo =/ s/= .*/= \"$(escape $DB_CONTAINER)\;3306\;$(escape $SERVER_DB_USER)\;$(escape $SERVER_DB_PWD)\;characters\"/" $CONFIG_PATH/worldserver.conf
+sed -i -e "/ArchiveDatabaseInfo   =/ s/= .*/= \"$(escape $DB_CONTAINER)\;3306\;$(escape $SERVER_DB_USER)\;$(escape $SERVER_DB_PWD)\;archive\"/" $CONFIG_PATH/worldserver.conf
+sed -i -e "/FusionCMSDatabaseInfo =/ s/= .*/= \"$(escape $DB_CONTAINER)\;3306\;$(escape $SERVER_DB_USER)\;$(escape $SERVER_DB_PWD)\;fusion\"/" $CONFIG_PATH/worldserver.conf
 
 sed -i -e "/GameType =/ s/= .*/= $(escape $GAME_TYPE)/" $CONFIG_PATH/worldserver.conf
 sed -i -e "/RealmZone =/ s/= .*/= $(escape $REALM_ZONE)/" $CONFIG_PATH/worldserver.conf
-sed -i -e "/Motd =/ s/= .*/= $(escape $MOTD_MSG)/" $CONFIG_PATH/worldserver.conf
+sed -i -e "/Motd =/ s/= .*/= \"$(escape $MOTD_MSG)\"/" $CONFIG_PATH/worldserver.conf
 
 sed -i -e "/Ra.Enable =/ s/= .*/= $(escape $RA_ENABLE)/" $CONFIG_PATH/worldserver.conf
-sed -i -e "/AHBot.Enable  =/ s/= .*/= $(escape $AH_ENABLE)/" $CONFIG_PATH/worldserver.conf
-sed -i -e "/AHBot.itemcount =/ s/= .*/= $(escape $AH_ITEM_COUNT)/" $CONFIG_PATH/worldserver.conf
 
 sed -i -e "/SOAP.Enabled =/ s/= .*/= $(escape $SOAP_ENABLE)/" $CONFIG_PATH/worldserver.conf
 sed -i -e "/SOAP.IP =/ s/= .*/= $(escape $SOAP_IP)/" $CONFIG_PATH/worldserver.conf
