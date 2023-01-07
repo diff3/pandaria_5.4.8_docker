ed6bd65be4899aea48d1e918eb80ff5faabcdd93
diff3 <magnus@entropy.nu>
2023-01-07 21:29:24 +0100

    compile entry

--- containers/compile/entry.sh
+++ containers/compile/entry.sh
@@ -34,26 +34,34 @@ cmake .. -DCMAKE_INSTALL_PREFIX=/opt/server -DCMAKE_C_COMPILER=/usr/bin/clang-11
 make clean
 make -j $(nproc) install
 
-# authserver.conf
-sed -i -e "/LogsDir =/ s/= .*/= $(escape $LOGS_DIR_PATH)/" $CONFIG_PATH/authserver.conf
-sed -i -e "/LoginDatabaseInfo =/ s/= .*/= \"$(escape $DB_CONTAINER)\;3306\;$(escape $SERVER_DB_USER)\;$(escape $SERVER_DB_PWD)\;realmd\"/" $CONFIG_PATH/authserver.conf
+if [ ! -f "/opt/server/etc/authserver.conf" ]; then
+   echo "updating authserver.conf files"
+   cp /opt/server/etc/authserver.conf.dist /opt/server/etc/authserver.conf
 
-# worldserver.conf
-sed -i -e "/DataDir =/ s/= .*/= $(escape $DATA_DIR_PATH)/" $CONFIG_PATH/worldserver.conf
-sed -i -e "/LogsDir =/ s/= .*/= $(escape $LOGS_DIR_PATH)/" $CONFIG_PATH/worldserver.conf
+   sed -i -e "/LogsDir =/ s/= .*/= $(escape $LOGS_DIR_PATH)/" $CONFIG_PATH/authserver.conf
+   sed -i -e "/LoginDatabaseInfo =/ s/= .*/= \"$(escape $DB_CONTAINER)\;3306\;$(escape $SERVER_DB_USER)\;$(escape $SERVER_DB_PWD)\;realmd\"/" $CONFIG_PATH/authserver.conf
+fi
 
-sed -i -e "/LoginDatabaseInfo     =/ s/= .*/= \"$(escape $DB_CONTAINER)\;3306\;$(escape $SERVER_DB_USER)\;$(escape $SERVER_DB_PWD)\;auth\"/" $CONFIG_PATH/worldserver.conf
-sed -i -e "/WorldDatabaseInfo     =/ s/= .*/= \"$(escape $DB_CONTAINER)\;3306\;$(escape $SERVER_DB_USER)\;$(escape $SERVER_DB_PWD)\;world\"/" $CONFIG_PATH/worldserver.conf
-sed -i -e "/CharacterDatabaseInfo =/ s/= .*/= \"$(escape $DB_CONTAINER)\;3306\;$(escape $SERVER_DB_USER)\;$(escape $SERVER_DB_PWD)\;characters\"/" $CONFIG_PATH/worldserver.conf
-sed -i -e "/ArchiveDatabaseInfo   =/ s/= .*/= \"$(escape $DB_CONTAINER)\;3306\;$(escape $SERVER_DB_USER)\;$(escape $SERVER_DB_PWD)\;archive\"/" $CONFIG_PATH/worldserver.conf
-sed -i -e "/FusionCMSDatabaseInfo =/ s/= .*/= \"$(escape $DB_CONTAINER)\;3306\;$(escape $SERVER_DB_USER)\;$(escape $SERVER_DB_PWD)\;fusion\"/" $CONFIG_PATH/worldserver.conf
+if [ ! -f "/opt/server/etc/authserver.conf" ]; then
+   echo "updating worldserver.conf files"
+   cp /opt/server/etc/worldserver.conf.dist /opt/server/etc/worldserver.conf
 
-sed -i -e "/GameType =/ s/= .*/= $(escape $GAME_TYPE)/" $CONFIG_PATH/worldserver.conf
-sed -i -e "/RealmZone =/ s/= .*/= $(escape $REALM_ZONE)/" $CONFIG_PATH/worldserver.conf
-sed -i -e "/Motd =/ s/= .*/= \"$(escape $MOTD_MSG)\"/" $CONFIG_PATH/worldserver.conf
+   sed -i -e "/DataDir =/ s/= .*/= $(escape $DATA_DIR_PATH)/" $CONFIG_PATH/worldserver.conf
+   sed -i -e "/LogsDir =/ s/= .*/= $(escape $LOGS_DIR_PATH)/" $CONFIG_PATH/worldserver.conf
 
-sed -i -e "/Ra.Enable =/ s/= .*/= $(escape $RA_ENABLE)/" $CONFIG_PATH/worldserver.conf
+   sed -i -e "/LoginDatabaseInfo     =/ s/= .*/= \"$(escape $DB_CONTAINER)\;3306\;$(escape $SERVER_DB_USER)\;$(escape $SERVER_DB_PWD)\;auth\"/" $CONFIG_PATH/worldserver.conf
+   sed -i -e "/WorldDatabaseInfo     =/ s/= .*/= \"$(escape $DB_CONTAINER)\;3306\;$(escape $SERVER_DB_USER)\;$(escape $SERVER_DB_PWD)\;world\"/" $CONFIG_PATH/worldserver.conf
+   sed -i -e "/CharacterDatabaseInfo =/ s/= .*/= \"$(escape $DB_CONTAINER)\;3306\;$(escape $SERVER_DB_USER)\;$(escape $SERVER_DB_PWD)\;characters\"/" $CONFIG_PATH/worldserver.conf
+   sed -i -e "/ArchiveDatabaseInfo   =/ s/= .*/= \"$(escape $DB_CONTAINER)\;3306\;$(escape $SERVER_DB_USER)\;$(escape $SERVER_DB_PWD)\;archive\"/" $CONFIG_PATH/worldserver.conf
+   sed -i -e "/FusionCMSDatabaseInfo =/ s/= .*/= \"$(escape $DB_CONTAINER)\;3306\;$(escape $SERVER_DB_USER)\;$(escape $SERVER_DB_PWD)\;fusion\"/" $CONFIG_PATH/worldserver.conf
 
-sed -i -e "/SOAP.Enabled =/ s/= .*/= $(escape $SOAP_ENABLE)/" $CONFIG_PATH/worldserver.conf
-sed -i -e "/SOAP.IP =/ s/= .*/= $(escape $SOAP_IP)/" $CONFIG_PATH/worldserver.conf
+   sed -i -e "/GameType =/ s/= .*/= $(escape $GAME_TYPE)/" $CONFIG_PATH/worldserver.conf
+   sed -i -e "/RealmZone =/ s/= .*/= $(escape $REALM_ZONE)/" $CONFIG_PATH/worldserver.conf
+   sed -i -e "/Motd =/ s/= .*/= \"$(escape $MOTD_MSG)\"/" $CONFIG_PATH/worldserver.conf
+
+   sed -i -e "/Ra.Enable =/ s/= .*/= $(escape $RA_ENABLE)/" $CONFIG_PATH/worldserver.conf
+
+   sed -i -e "/SOAP.Enabled =/ s/= .*/= $(escape $SOAP_ENABLE)/" $CONFIG_PATH/worldserver.conf
+   sed -i -e "/SOAP.IP =/ s/= .*/= $(escape $SOAP_IP)/" $CONFIG_PATH/worldserver.conf
+fi
 
