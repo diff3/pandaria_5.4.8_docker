f4019cc36e6bbbe186347ecb15ae21282cad1501
Magnus Pettersson <magnus@entropy.nu>
2023-02-10 09:07:38 +0100

    minor changes to different containers

--- containers/compile/entry.sh
+++ containers/compile/entry.sh
@@ -18,10 +18,6 @@ if [ ! -d "/opt/server/logs" ]; then
    mkdir -p /opt/server/logs
 fi
 
-if [ ! -d "/opt/server/honor" ]; then
-   mkdir -p /opt/server/honor
-fi
-
 if [ ! -d "/opt/server/etc" ]; then
    mkdir -p /opt/server/etc
 fi
@@ -29,7 +25,7 @@ fi
 
 cd /opt/etc/pandaria_5.4.8/build
 
-cmake .. -DCMAKE_INSTALL_PREFIX=/opt/server -DCMAKE_C_COMPILER=/usr/bin/clang-11 -DCMAKE_CXX_COMPILER=/usr/bin/clang++-11 -DSCRIPTS=static -DWITH_WARNINGS=0 -DTOOLS=1 -DCMAKE_CXX_FLAGS=-pthread
+cmake .. -DCMAKE_INSTALL_PREFIX=/opt/server -DCMAKE_C_COMPILER=/usr/bin/clang-11 -DCMAKE_CXX_COMPILER=/usr/bin/clang++-11 -DSCRIPTS=static -DWITH_WARNINGS=0 -DTOOLS=0 -DCMAKE_CXX_FLAGS=-pthread
 
 make clean
 make -j $(nproc) install
